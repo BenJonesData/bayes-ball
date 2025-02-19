02465fd61af5d2bd2bf89c92d42cc6c82c6c0980
Ben Jones <benjones1479@gmail.com>
2025-02-19 09:37:55 +0000

    feat: functionise experimentation
    training data and validation data will be passed in seperately and optional tags will be added.

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 poetry run python src/experiment.py \
-    --training_data_path 'data/train_val_data.csv' \
+    --training_data_path 'data/train_data.csv' \
+    --validation_data_path 'data/val_data.csv'\
     --learning_rate 0.001 \
     --num_epochs 10000 \
     --num_samples 1000 \
